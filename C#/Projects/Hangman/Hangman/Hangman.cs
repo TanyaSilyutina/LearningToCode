8571a6179d2542f1fd2ba39e103ef34775d82cc2
TanyaSilyutina <silyutinatanya@gmail.com>
2019-03-11 03:49:08 -0700

    Hangman (In Progress)

---
+++ C#/Projects/Hangman/Hangman/Hangman.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace Hangman
+{
+    public class Hangman
+    {
+        public bool HasWon = false;
+        public int Attempts = 8;
+        private string word;
+        private char[] toBeFilledWord;
+        private List<char> usedLetters;
+
+        public Hangman(string word)
+        {
+            this.word = word;
+            this.toBeFilledWord = new char[word.Length];
+            usedLetters = new List<char>();
+
+            for (int i = 0; i < this.toBeFilledWord.Length; ++i)
+            {
+                this.toBeFilledWord[i] = '_';
+            }
+        }
+
+        public string GetWordToBeDisplayed()
+        {
+            return this.ConvertToDisplayString(this.toBeFilledWord);
+        }
+
+        public string GetUsedLettersToBeDisplayed()
+        {
+            return this.ConvertToDisplayString(this.usedLetters.ToArray());
+        }
+
+        public void LetterExistsCheck(char letter)
+        {
+            bool letterExists = false;
+
+            for (int i = 0; i < this.word.Length; i++)
+            {
+                if (this.word[i] == letter)
+                {
+                    this.toBeFilledWord[i] = letter;
+                    letterExists = true;
+                }
+            }
+
+            if (!letterExists)
+            {
+                --this.Attempts;
+                this.usedLetters.Add(letter);
+            }
+        }
+
+        public bool IsGameOver()
+        {
+            if (Attempts == 0 || this.word == new string(this.toBeFilledWord))
+            {
+                if(Attempts == 0)
+                    this.HasWon = false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private string ConvertToDisplayString(char[] word)
+        {
+            string displayWord = string.Empty;
+
+            for (int i = 0; i < word.Length; ++i)
+            {
+                displayWord += (word[i] + " ");
+            }
+
+            return displayWord;
+        }
+    }
+}
---
+++ C#/Projects/Hangman/Hangman/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to hangman!\n");
+            Console.Write("Enter your secret word: ");
+            string wordInput = Console.ReadLine();
+
+            Hangman hangmanObject = new Hangman(wordInput);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Guess the word!\n");
+                Console.WriteLine(hangmanObject.GetWordToBeDisplayed() + "\n");
+                Console.WriteLine("Words Used: " + hangmanObject.GetUsedLettersToBeDisplayed());
+                Console.WriteLine("Attempts Remaining: " + hangmanObject.Attempts + "\n");
+
+                if(hangmanObject.IsGameOver())
+                    break;
+
+                Console.Write("Enter your guess: ");
+                char enteredLetter = Console.ReadKey().KeyChar;
+
+                hangmanObject.LetterExistsCheck(enteredLetter);
+            }
+
+            if (hangmanObject.HasWon == true)
+                Console.WriteLine("\nYou have won the game! :)");
+            else
+                Console.WriteLine("\nYou have lost the game! :(");
+        }
+    }
+}
