ccb565815daf9be90421895d81d771a4fdf07eda
TanyaSilyutina <silyutinatanya@gmail.com>
2019-03-11 03:20:45 -0700

    TicTacToe (In Progress)

---
+++ C#/Projects/TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace TicTacToe
+{
+  class Board
+  {
+    // 2D array to hold board values
+    private char [,] board;
+    
+    // Flag to toggle between 'X' and 'O'
+    public bool toggle;
+
+    // Constructor of board class
+    public Board()
+    {
+      // Initializing 3 by 3 2D board array
+      this.board = new char[3,3]
+      {
+        { ' ', ' ', ' '},
+	    { ' ', ' ', ' '},
+	    { ' ', ' ', ' '}
+      };
+
+      // Give the toggle a starting value
+      toggle = true;
+    }
+
+    // Draws the TicTacToe board
+    public void DrawBoard()
+    {
+      Console.WriteLine("\nWelcome to Tic-Tac-Toe!\n");
+      Console.WriteLine("Enter your input in this format: RowColumn");
+      Console.WriteLine("Example: 01\n");
+
+      Console.WriteLine("     |     |     ");
+      Console.WriteLine("  {0}  |  {1}  |  {2}  ", this.board[0, 0], this.board[0, 1], this.board[0, 2]);
+      Console.WriteLine("_____|_____|_____");
+      Console.WriteLine("     |     |     ");
+      Console.WriteLine("  {0}  |  {1}  |  {2}  ", this.board[1, 0], this.board[1, 1], this.board[1, 2]);
+      Console.WriteLine("_____|_____|_____");
+      Console.WriteLine("     |     |     ");
+      Console.WriteLine("  {0}  |  {1}  |  {2}  ", this.board[2, 0], this.board[2, 1], this.board[2, 2]);
+      Console.WriteLine("     |     |     \n");
+    }
+
+    public void ExecuteTurn(int row, int column)
+    {
+      // Print X and invert the toggle
+      if (this.toggle == true)
+      {
+        this.board[row, column] = 'X';
+        this.toggle = !this.toggle;
+      }
+      // Print O and invert the toggle
+      else
+      {
+        this.board[row, column] = 'O';
+        this.toggle = !this.toggle;
+      }
+
+      Console.Clear();
+      this.DrawBoard();
+    }
+
+    // Checks the winning condition
+    public bool CheckWin()
+    {
+      if (this.CheckWinDiagonals() || this.CheckWinRows() || this.CheckWinColumns())      
+      {
+        return true;
+      }
+      
+      return false;
+    }
+
+    public bool CheckDraw()
+    {
+      for (int i = 0; i < this.board.GetLength(0); i++)
+      {
+        for (int j = 0; j < this.board.GetLength(1); j++)
+        {
+          if (this.board[i, j] == ' ')
+          {
+            return false;
+          }          
+        }
+      }
+
+      return true;
+    }
+
+    // Checks the diagonals for a winning condition
+    private bool CheckWinDiagonals()
+    {
+      if ((this.board[0, 0] == this.board[1, 1] && this.board[0, 0] == this.board[2, 2]) && this.board[0, 0] != ' ' ||
+          (this.board[0, 2] == this.board[1, 1] && this.board[0, 0] == this.board[2, 0]) && this.board[0, 2] != ' ')
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+    // Checks the rows for a winning condition
+    private bool CheckWinRows()
+    {
+      if ((this.board[0, 0] == this.board[0, 1] && this.board[0, 0] == this.board[0, 2]) && this.board[0, 0] != ' ' ||
+          (this.board[1, 0] == this.board[1, 1] && this.board[1, 0] == this.board[1, 2]) && this.board[1, 0] != ' ' ||
+          (this.board[2, 0] == this.board[2, 1] && this.board[2, 0] == this.board[2, 2]) && this.board[2, 0] != ' ' )
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+    // Checks the columns for a winning condition
+    private bool CheckWinColumns()
+    {
+      if ((this.board[0, 0] == this.board[1, 0] && this.board[0, 0] == this.board[2, 0]) && this.board[0, 0] != ' ' ||
+          (this.board[0, 1] == this.board[1, 1] && this.board[0, 1] == this.board[2, 1]) && this.board[0, 1] != ' ' ||
+          (this.board[0, 2] == this.board[1, 2] && this.board[0, 2] == this.board[2, 2]) && this.board[0, 2] != ' ')
+      {
+        return true;
+      }
+
+      return false;
+    }
+  }
+}
---
+++ C#/Projects/TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,46 @@
+﻿using TicTacToe;
+using System;
+
+namespace Progam
+{
+  class Program
+  {
+    static void Main(string[] args)
+	{
+      // Create a new board object
+      Board boardObject = new Board();
+
+      // Draws the board
+      boardObject.DrawBoard();
+
+      while (!boardObject.CheckWin())
+      {
+        if (boardObject.toggle == true)
+        {
+          Console.Write("Player 1, select your position (X):  ");
+        }
+        else
+        {
+          Console.Write("Player 2, select your position (O):  ");
+        }
+
+        // Console.Read() returns an ASCII value. 
+        // '0' has an ASCII value of 48.
+        // Subtracting the read value from '0' will give you the number.
+        // Example: '1' (ASCII Value 49) - '0' (ASCII Value 48) = 49 - 48 = 1
+        int rowInput = Program.ReadKey();
+        int columnInput = Program.ReadKey();
+
+        // Pass in the user entered row and column values to the Board class
+        boardObject.ExecuteTurn(rowInput, columnInput);
+      }
+      
+      Console.WriteLine("You have won.");
+    }
+
+    static int ReadKey()
+    {
+      return Console.ReadKey().KeyChar - '0';
+    }
+  }
+}
